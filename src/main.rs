927bb0c3176ba3ecf132ba9cd4c433cfdad6e4dd
alg747 <alg747@protonmail.com>
2025-02-01 16:34:45 +0000

    read contents of file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -6,8 +6,12 @@ fn main() {
     let query = &args[1];
     let file_path = &args[2];
 
-    println!("Searching for: '{}'", query);
-    println!("In the file: '{file_path}'");
+    println!("Searching for:\t\t'{}'", query);
+    println!("In the file:\t\t'{file_path}'");
 
     // dbg!(args);
+
+    let contents = fs::read_to_string(file_path).expect("Something went wrong reading the file");
+
+    println!("With the text:\n---\n{contents}---");
 }
